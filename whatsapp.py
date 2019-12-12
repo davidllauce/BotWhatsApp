2554d34f6556c13f0bd171f81968603aae02fdfb
David llauce santos <davidllaucesantos@gmail.com>
2019-12-11 21:04:22 -0500

    first commit

---
+++ whatsapp.py
@@ -0,0 +1,59 @@
+import socket
+from time import sleep
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+message_text = 'Te observamos :)'  # message you want to send
+no_of_message = 1  # no. of time you want the message to be send
+moblie_no_list = [51993495670]  # list of phone number can be of any length
+
+
+def element_presence(by, xpath, time):
+    element_present = EC.presence_of_element_located((By.XPATH, xpath))
+    WebDriverWait(driver, time).until(element_present)
+
+
+def is_connected():
+    try:
+        # connect to the host -- tells us if the host is actually
+        # reachable
+        socket.create_connection(("www.google.com", 80))
+        return True
+    except:
+        is_connected()
+
+
+driver = webdriver.Chrome('chromedriver')
+driver.get("http://web.whatsapp.com")
+sleep(10)  # wait time to scan the code in second
+
+
+def send_whatsapp_msg(phone_no, text):
+    driver.get("https://web.whatsapp.com/send?phone={}&source=&data=#".format(phone_no))
+    try:
+        driver.switch_to_alert().accept()
+    except Exception as e:
+        pass
+
+    try:
+        element_presence(By.XPATH, '//*[@id="main"]/footer/div[1]/div[2]/div/div[2]', 30)
+        txt_box = driver.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')
+        global no_of_message
+        for x in range(no_of_message):
+            txt_box.send_keys(text)
+            txt_box.send_keys("\n")
+
+    except Exception as e:
+        print("Teléfono no encontrado:" + str(phone_no))
+
+
+for moblie_no in moblie_no_list:
+    try:
+        send_whatsapp_msg(moblie_no, message_text)
+
+    except Exception as e:
+        sleep(10)
+        is_connected()
